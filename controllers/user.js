9d33c80f23b8b2cc6998054c72d1835e3d713771
aarticontractor <contaaru@gmail.com>
2023-05-20 17:36:20 -0700

    Get API for all users

--- controllers/user.js
+++ controllers/user.js
@@ -13,4 +13,14 @@ router.post('/', async (req, res) => {
     }
 });
 
+// GET all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
