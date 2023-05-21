b2e1b4f4c1759314bad2310df4fe14197925c9df
aarticontractor <contaaru@gmail.com>
2023-05-20 22:44:36 -0700

    created GET routes to get all thoughts and by ID

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -10,12 +10,10 @@ router.post('/', async (req, res) => {
     }
 
     try {
-        const newThought = new Thought(req.body);
-        const savedThought = await newThought.save();
-
+        const savedThought = await Thought.create(req.body);
         await User.findByIdAndUpdate(req.body.userId, { $push: { thoughts: savedThought._id } }, { new: true });
 
-        res.status(201).json(savedThought.toObject());
+        res.status(201).json(savedThought);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -28,10 +26,38 @@ router.put('/:id', async (req, res) => {
         if (!updatedThought) {
             return res.status(404).json({ message: 'No thought found with this id!' });
         }
-        res.json(updatedThought.toObject());
+        res.json(updatedThought);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// GET all thoughts
+router.get('/', async (req, res) => {
+    try {
+        const thoughts = await Thought.find({});
+        res.status(200).json(thoughts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// GET a single thought by its _id
+router.get('/:id', async (req, res) => {
+    try {
+        const thought = await Thought.findById(req.params.id);
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought found with this id!' });
+        }
+        res.json(thought);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+// get: createdAtVal => dateFormat(createdAtVal)
+
 
 module.exports = router;
