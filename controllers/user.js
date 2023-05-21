256508f85a3d23f67144fcb653e1192b4ed1d931
aarticontractor <contaaru@gmail.com>
2023-05-20 17:36:39 -0700

    Get API for specific user by Id

--- controllers/user.js
+++ controllers/user.js
@@ -23,4 +23,17 @@ router.get('/', async (req, res) => {
     }
 });
 
+// GET a single user by its _id and populated thought and friend data
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+        if (!user) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
