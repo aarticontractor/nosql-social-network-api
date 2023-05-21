95c0daf8355860fd32d55123e0386f978c42e804
aarticontractor <contaaru@gmail.com>
2023-05-20 17:47:49 -0700

    POST API to add a new friend - it will be added on both users list

--- controllers/user.js
+++ controllers/user.js
@@ -36,4 +36,29 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// POST API to add a new friend to a user's friend list
+router.post('/:userId/friends/:friendId', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.userId);
+        const friend = await User.findById(req.params.friendId);
+
+        if (!user || !friend) {
+            return res.status(404).json({ message: 'No user found with this id!' });
+        }
+
+        if (user.friends.includes(req.params.friendId) || friend.friends.includes(req.params.userId)) {
+            return res.status(400).json({ message: 'This friend is already added!' });
+        }
+
+        user.friends.push(req.params.friendId);
+        friend.friends.push(req.params.userId);
+        await user.save();
+        await friend.save();
+
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
